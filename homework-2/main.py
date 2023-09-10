528532e4e7fb377fa9412f5f750db934182f441b
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-09-10 16:39:27 +0300

    work 2 ready

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,11 +2,17 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    """добавил вызов сюда, если вызвать в конце файла - json не создаётся
+    причины этого не понял + кодировку utf-8 указал, но кириллица почему-то 
+    не отображается корректно
+    """
+    moscowpython.to_json('moscowpython.json')
 
     # получаем значения атрибутов
     print(moscowpython.title)  # MoscowPython
     print(moscowpython.video_count)  # 685 (может уже больше)
-    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+    print(moscowpython.url)
+    # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
     moscowpython.channel_id = 'Новое название'
@@ -17,4 +23,6 @@ if __name__ == '__main__':
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
     # создаем файл 'moscowpython.json' в данными по каналу
+    # moscowpython.to_json('moscowpython.json')
+
     moscowpython.to_json('moscowpython.json')
--- src/channel.py
+++ src/channel.py
@@ -13,14 +13,46 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут
         подтягиваться по API
         """
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+        response = (self.get_service().channels().
+                    list(id=self.__channel_id,
+                         part='snippet,statistics').execute().get('items')[0])
+
+        self.title = response.get('snippet').get('title')
+        self.description = response.get('snippet').get('description')
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscriberCount = response.get('statistics').get('subscriberCount')
+        self.video_count = response.get('statistics').get('videoCount')
+        self.viewCount = response.get('statistics').get('viewCount')
+
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3',
+                        developerKey=api_key)
+        return youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """
         Выводит в консоль информацию о канале
         """
-        youtube = build('youtube', 'v3', developerKey=api_key)
-
-        channel = youtube.channels().list(id=self.channel_id,
-                                          part='snippet,statistics').execute()
+        channel = (self.get_service().channels().
+                   list(id=self.__channel_id,
+                        part='snippet,statistics').execute())
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, filename):
+        channel_data = {
+            'id': self.__channel_id,
+            'name': self.title,
+            'link': self.url,
+            'description': self.description,
+            'view_count': self.viewCount,
+            'subscriber_count': self.subscriberCount
+        }
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(channel_data, file)
